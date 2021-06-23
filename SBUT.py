3a6f81f04ab22f471f8cf474a66e428ca8ab6d13
y-z-zhang <zhangyuanzhao45@gmail.com>
2021-06-22 22:21:00 -0500

    Update SBUT.py

--- SBUT.py
+++ SBUT.py
@@ -21,21 +21,22 @@ C2 = np.diag([0, 0, 0, 1, 1, 1, 1])
 L = np.diag(np.sum(A, axis=1)) - A
 
 # In[]
-# Generate a random combination M of all generating matrices so we don't have to worry about degeneracy (or use the form suggested in the paper for better alignment with clusters)
+# Generate a random combination M of all generating matrices
 M = Matrix(2*C1@L@C1 + 5*C2@L@C2)
 
 # In[]
 # Find the basis for the common block upper triangular form by finding the Jordan decomposition of M
 P, J = M.jordan_form()
 TempL = P.inv()*L*P
+pprint(J)
 
 # In[]
-# Use the BlockSort algorithm to reorder the basis P so that it can be used to reveal the common block upper triangular form
+# Reorder the basis P so that it reveals the common block upper triangular form
 order = [0,3,4,5,6,1,2]
 FinalL = TempL[order,order]
-FinalB = P[:,order].simplify()
+FinalB = P[:,order]#.simplify()
 pprint(FinalL)
-print(FinalB)
+pprint(FinalB)
 
 
 # <md>
